c52420e6cce92a23802cc7d342f3f1a467c8f038
Constantin Cristian Florea <floreact@gmail.com>
2022-04-15 20:06:56 +0100

    Changed config settings for Heroku

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 module.exports = {
     "development": {
-        "username": process.env.DB_USERNAME,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
+        "username": DB_USERNAME,
+        "password": DB_PASSWORD,
+        "database": DB_NAME,
+        "host": DB_HOST,
         "dialect": "mysql"
     },
     "test": {
