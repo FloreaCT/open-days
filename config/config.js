de6df105af5bb8e3b19c69f279d925079bb149cd
Constantin Cristian Florea <floreact@gmail.com>
2022-04-15 21:32:37 +0100

    Testing Heroku

--- config/config.js
+++ config/config.js
@@ -1,24 +1,18 @@
 require('dotenv').config()
 module.exports = {
     "development": {
-        "username": DB_USERNAME,
-        "password": DB_PASSWORD,
-        "database": DB_NAME,
-        "host": DB_HOST,
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
         "dialect": "mysql"
-    },
-    "test": {
-        "username": "FloreaCT",
-        "password": "qwer",
-        "database": "database_test",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
+
     },
     "production": {
-        "username": "Floreact",
-        "password": "qwer",
-        "database": "database_production",
-        "host": "127.0.0.1",
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
         "dialect": "mysql"
     }
 }
